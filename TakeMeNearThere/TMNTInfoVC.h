841a1d27812b50e5ef63774f030f9927a4bfa029
Ian Blue <ianrblue@gmail.com>
2013-03-27 12:01:26 -0500

    added info viewcontroller

---
+++ TakeMeNearThere/TMNTInfoVC.h
@@ -0,0 +1,13 @@
+//
+//  TMNTInfoVC.h
+//  TakeMeNearThere
+//
+//  Created by Ian Blue on 3/27/13.
+//  Copyright (c) 2013 Heroes in a Half Shell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMNTInfoVC : UIViewController
+
+@end
